b7e372d587102f51a548c4fc97379cb9a3c84311
seochangwook <scw05313315@gmail.com>
2019-10-22 15:55:21 +0900

    scw(19/10/22) - project init

---
+++ LOTTEHOTELFMS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LOTTEHOTELFMS
+//
+//  Created by ldcc on 2019/10/22.
+//  Copyright © 2019 ldcc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
