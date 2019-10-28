2a747410a31ba0670fe376c2d8764af14f67e83a
seochangwook <scw05313315@gmail.com>
2019-10-28 18:19:40 +0900

    loading, setting, mainwebview add, networkcheck add

---
+++ LOTTEHOTELFMS/Controller/MainWebViewController.swift
@@ -0,0 +1,26 @@
+//
+//  MainWebViewController.swift
+//  LOTTEHOTELFMS
+//
+//  Created by ldcc on 2019/10/28.
+//  Copyright © 2019 ldcc. All rights reserved.
+//
+
+import UIKit
+
+class MainWebViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
